828be428df65a9d3ac15e8c67ed353e356f48f9b
ariefnurputranto <arief@qiscus.co>
2020-07-14 21:29:29 +0700

    implement sample carthage

--- Examples/carthage/Example/ViewController.swift
+++ Examples/carthage/Example/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import QiscusCore
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        QiscusCore.setup(WithAppID: "yourAppID")
+        QiscusCore.enableDebugPrint = true
+        
+        QiscusCore.loginOrRegister(userID: "arief92", userKey: "arief92", onSuccess: { (user) in
+            print("user: \(user.username)")
+            
+            QiscusCore.shared.chatUser(userId: "arief93", onSuccess: { (room, comments) in
+                
+            }) { (error) in
+                
+            }
+            
+            QiscusCore.shared.getAllChatRooms(page: 1, limit: 100, onSuccess: { (rooms, meta) in
+                
+            }) { (error) in
+                
+            }
+            
+        }) { (error) in
+            print(error.message)
+        }
     }
 
 
