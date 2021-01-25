079b1607845f61ec87acf6c0665708b5e4e26ef6
ariefnurputranto <arief@qiscus.co>
2021-01-25 16:12:37 +0700

    bump up version 1.5.10 and increase version QiscusRealtime

--- Source/QiscusCore/QiscusCore.swift
+++ Source/QiscusCore/QiscusCore.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public class QiscusCore: NSObject {
-    public static let qiscusCoreVersionNumber:String = "1.5.9"
+    public static let qiscusCoreVersionNumber:String = "1.5.10"
     class var bundle:Bundle{
         get{
             let podBundle = Bundle(for: QiscusCore.self)
