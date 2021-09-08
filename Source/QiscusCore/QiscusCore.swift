e8fe5124d60e02d7f67da893e2f7add42f619d02
ariefnurputranto <arief@qiscus.co>
2021-09-08 09:08:17 +0700

    bump up version 3.0.0-beta.11

--- Source/QiscusCore/QiscusCore.swift
+++ Source/QiscusCore/QiscusCore.swift
@@ -10,7 +10,7 @@ import Foundation
 import QiscusRealtime
 import CoreData
 public class QiscusCore: NSObject {
-    public static let qiscusCoreVersionNumber:String = "3.0.0-beta.10"
+    public static let qiscusCoreVersionNumber:String = "3.0.0-beta.11"
     class var bundle:Bundle{
         get{
             let podBundle = Bundle(for: QiscusCore.self)
