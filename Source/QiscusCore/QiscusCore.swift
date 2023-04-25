4c1fa2bf6237fd61674859b7bf416c7a3723b24f
Arief Nur Putranto <arief@qiscus.co>
2023-04-25 09:02:12 +0700

    make to public var

--- Source/QiscusCore/QiscusCore.swift
+++ Source/QiscusCore/QiscusCore.swift
@@ -43,7 +43,7 @@ public class QiscusCore: NSObject {
         }
     }
     
-    var reachability:QiscusReachability?
+   public var reachability:QiscusReachability?
     
     class func setupReachability(){
         QiscusCore.shared.reachability = QiscusReachability()
