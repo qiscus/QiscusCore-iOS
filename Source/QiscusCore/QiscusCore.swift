e907e5fd656f156fc1e81adf9da32f077b6786ca
Arief Nur Putranto <arief@qiscus.co>
2025-05-23 12:54:04 +0700

    support SPM

--- Source/QiscusCore/QiscusCore.swift
+++ Source/QiscusCore/QiscusCore.swift
@@ -11,7 +11,7 @@ import QiscusRealtime
 import CoreData
 import UIKit
 public class QiscusCore: NSObject {
-    public static let qiscusCoreVersionNumber:String = "3.0.0-beta.17"
+    public static let qiscusCoreVersionNumber:String = "3.0.0-beta.18"
     class var bundle:Bundle{
         get{
             let podBundle = Bundle(for: QiscusCore.self)
